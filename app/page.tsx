37ade67903b4f2cd04d294a98d351fd5b0ae49e6
Karel <karelos101@gmail.com>
2024-08-16 11:57:37 +0200

    its working

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,128 @@
-import Image from "next/image";
+"use client"
+
+import { roboto } from "./fonts"
+import { useState, useRef } from "react"
+import ReactMarkdown from "react-markdown"
+import rehypeHighlight from "rehype-highlight"
+import "highlight.js/styles/github-dark.css"
+
+function tryParseJSONObject(jsonString: string) {
+  try {
+    var o = JSON.parse(jsonString)
+
+    // Handle non-exception-throwing cases:
+    // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+    // but... JSON.parse(null) returns null, and typeof null === "object",
+    // so we must check for that, too. Thankfully, null is falsy, so this suffices:
+    if (o && typeof o === "object") {
+      return o
+    }
+  } catch (e) {}
+
+  return false
+}
 
 export default function Home() {
+  const [responseText, setResponseText] = useState("")
+  const prevTextRef = useRef("")
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className={`${roboto.className} antialiased`}>
+      <div className='m-0 pulse flex justify-center items-center'>
+        <div className='w-10/12 mt-8 mb-32 rounded-2xl shadow-md align-baseline bg-slate-900 min-h-wfa'>
+          <div className='text-white text-justify self-baseline mt-2 mb-6 mx-6 mh-80vh'>
+            <ReactMarkdown rehypePlugins={[rehypeHighlight]}>
+              {responseText}
+            </ReactMarkdown>
+          </div>
+        </div>
+        <div className='fixed bottom-0 z-50 w-10/12 max-w-3xl bg-transparent pb-10'>
+          <form
+            className='flex justify-between items-end w-full bg-gray-900 rounded-2xl py-2.5 px-5 border-none-on-focus'
+            action={async (FormData: FormData) => {
+              "use client"
+              const message = FormData.get("chat-input")
+              let pervText = prevTextRef.current
+
+              const body = {
+                model: "llama3.1",
+                messages: [
+                  {
+                    role: "user",
+                    content: message,
+                  },
+                ],
+              }
+
+              const response = await fetch("http://localhost:11434/api/chat", {
+                method: "POST",
+                body: JSON.stringify(body),
+              })
+              const reader = response.body?.getReader()
+              if (!reader) {
+                throw new Error("Failed to read response body")
+              }
+
+              let newText = pervText ? pervText + "\n\n" : ""
+              newText += `# User: \n ${message} \n # AI:\n`
+              setResponseText(newText)
+
+              while (true) {
+                const { done, value } = await reader.read()
+                if (done) {
+                  prevTextRef.current = newText + "\n"
+                  break
+                }
+                const raw_json = new TextDecoder().decode(value)
+                const json = tryParseJSONObject(raw_json)
+
+                if (!json) {
+                  let raw_jsons = raw_json.split("\n")
+                  raw_jsons.pop()
+
+                  for (let raw_json of raw_jsons) {
+                    const json = JSON.parse(raw_json)
+                    newText += json.message.content
+                    setResponseText(newText)
+                  }
+                } else {
+                  newText += json.message.content
+                  setResponseText(newText)
+                }
+              }
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+            <input
+              type='text'
+              id='chat-input'
+              placeholder='Type your message here...'
+              autoComplete='off'
+              name='chat-input'
+              required
+              className='flex-1 bg-transparent border-none-on-focus text-white p-2.5'
             />
-          </a>
+            <button
+              type='submit'
+              className='bg-sky-800 text-black border-none-on-focus rounded-xl size-10 cursor-pointer self-center'
+            >
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='send-icon'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5'
+                />
+              </svg>
+            </button>
+          </form>
         </div>
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  );
+  )
 }
